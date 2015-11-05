de9bee6b2b5ef21b84bc0e96178d87ce6616a55f
ColinKohler <colink78@gmail.com>
2015-11-05 03:54:07 -0500

    Added more spooky scary skeleton code for account creation and setting preferences. Updated error handeling on login page.

---
+++ app/src/main/java/justbe/mindfulnessapp/CreateAccountActivity.java
@@ -0,0 +1,24 @@
+package justbe.mindfulnessapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+    }
+
+    public void createAccountPressed(View view) {
+        Intent intent = new Intent(this, PreferencesActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/justbe/mindfulnessapp/LoginActivity.java
+++ app/src/main/java/justbe/mindfulnessapp/LoginActivity.java
@@ -3,27 +3,51 @@ package justbe.mindfulnessapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
     public final static String EXTRA_USERNAME = "justbe.mindfulnessapp.USERNAME";
 
+    private EditText username_field;
+    private EditText password_field;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        username_field = (EditText) findViewById(R.id.editUsername);
+        username_field.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                EditText password_field = (EditText) findViewById(R.id.editPassword);
+                password_field.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
     }
 
-    public void login_pushed(View view) {
+    public void loginPressed(View view) {
         // Get the username and password from their respective fields
-        EditText username_field = (EditText) findViewById(R.id.editUsername);
-        EditText password_field = (EditText) findViewById(R.id.editPassword);
+        username_field = (EditText) findViewById(R.id.editUsername);
+        password_field = (EditText) findViewById(R.id.editPassword);
         String username = username_field.getText().toString();
         String password = password_field.getText().toString();
 
         login(username, password);
+    }
+
+    public void createAccountPressed(View view) {
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 
     // TODO: Authenticate login
@@ -33,7 +57,9 @@ public class LoginActivity extends AppCompatActivity {
             intent.putExtra(EXTRA_USERNAME, username);
             startActivity(intent);
         } else {
-            // REJECTED!
+            password_field = (EditText) findViewById(R.id.editPassword);
+            password_field.setError("Password and username didn't match");
         }
     }
 }
+
