730d9bbbebfb3e6dda58b12a3a21a5f57999f48b
ColinKohler <colink78@gmail.com>
2015-11-12 18:43:14 -0500

    Modified margins, Lesson View, Create account view
    Changed margins to be in dimens.xml. Added scroll view to the lesson
    view and filled it in with garbage text. Added a check to makes sure the
    passwords on the Create Account View are the same.

--- app/src/main/java/justbe/mindfulnessapp/CreateAccountActivity.java
+++ app/src/main/java/justbe/mindfulnessapp/CreateAccountActivity.java
@@ -4,9 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    private EditText username_field;
+    private EditText password_field;
+    private EditText confirm_password_field;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +23,40 @@ public class CreateAccountActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setTitle(getString(R.string.title_activity_create_account));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        confirm_password_field = (EditText) findViewById(R.id.editPassword);
+        confirm_password_field.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                EditText confirm_password_field = (EditText) findViewById(R.id.editConfirmPassword);
+                confirm_password_field.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
     }
 
     public void createAccountPressed(View view) {
-        Intent intent = new Intent(this, GettingStartedActivity.class);
-        startActivity(intent);
+        if(samePassword()) {
+            Intent intent = new Intent(this, GettingStartedActivity.class);
+            startActivity(intent);
+        } else {
+            confirm_password_field = (EditText) findViewById(R.id.editConfirmPassword);
+            confirm_password_field.setError("Passwords didn't match");
+        }
+    }
+
+    private boolean samePassword() {
+        password_field         = (EditText) findViewById(R.id.editPassword);
+        confirm_password_field = (EditText) findViewById(R.id.editConfirmPassword);
+        return password_field.getText().toString().equals(confirm_password_field.getText().toString());
     }
 }
--- app/src/main/java/justbe/mindfulnessapp/GettingStartedActivity.java
+++ app/src/main/java/justbe/mindfulnessapp/GettingStartedActivity.java
@@ -14,11 +14,13 @@ public class GettingStartedActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_preferences);
+        setContentView(R.layout.activity_getting_started);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setTitle(getString(R.string.title_activity_getting_started));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
 }
