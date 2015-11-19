ae8a6c738e4c333618fc1b3ca4087ae100434dcf
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-11-19 01:12:09 -0500

    Create test model

---
+++ app/src/main/java/justbe/mindfulnessapp/Models/CreateUserResponse.java
@@ -0,0 +1,20 @@
+package justbe.mindfulnessapp.Models;
+
+/**
+ * Created by eddiehurtig on 11/19/15.
+ */
+public class CreateUserResponse {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+
+}
