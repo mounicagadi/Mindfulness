2130ec59cc6dd599923be2ae6c1c0e226a27b81e
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-11-20 12:16:44 -0500

    Clean up a bit

--- app/src/main/java/justbe/mindfulnessapp/HttpRequestTask.java
+++ app/src/main/java/justbe/mindfulnessapp/HttpRequestTask.java
@@ -3,6 +3,9 @@ package justbe.mindfulnessapp;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +13,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.core.ParameterizedTypeReference;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -17,7 +23,7 @@ import java.util.Map;
  * Generic HTTP Request Async Task. Takes Request parameters in doInBackground and a Type Parameter
  * to define the model to parse the expected JSON HTTP response as.
  */
-public class HttpRequestTask<T> extends AsyncTask<Object, Void, ResponseEntity<ResponseWrapper<T>>> {
+public class HttpRequestTask<S, T> extends AsyncTask<Object, Void, ResponseEntity<ResponseWrapper<T>>> {
     @Override
     protected ResponseEntity<ResponseWrapper<T>> doInBackground(Object... params) {
         try {
@@ -27,29 +33,44 @@ public class HttpRequestTask<T> extends AsyncTask<Object, Void, ResponseEntity<R
             HttpMethod method = HttpMethod.GET;
 
             if (params.length >= 2) {
-                method = HttpMethod.valueOf((String) params[1]);
+                if (params[1] instanceof HttpMethod) {
+                    method = (HttpMethod) params[1];
+                } else {
+                    method = HttpMethod.valueOf((String) params[1]);
+                }
             }
 
-            String body = "";
+            S body = null;
             if (params.length >= 3) {
-                if (params[2] instanceof Map<?, ?>) {
-                    for (String key : ((Map<String, String>) params[2]).keySet()) {
-                        body += key + ": " + ((Map<String, String>) params[2]).get(key);
-                    }
-                }
+                body = (S) params[2];
             }
 
             RestTemplate restTemplate = new RestTemplate();
             restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-            Type t = String.class;
 
+            List<MediaType> acceptableMediaTypes = new ArrayList();
+            acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
+
+            HttpHeaders headers = new HttpHeaders();
+
+            headers.setAccept(acceptableMediaTypes);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            
+            // body
+            HttpEntity<S> entity = new HttpEntity<S>(body, headers);
+
+            Map<String, Object> uriVariables = new HashMap<String, Object>();
+            uriVariables.put("content", "foobazzeee");
+
+            // Send the request
             ResponseEntity<ResponseWrapper<T>> response = restTemplate.exchange(
                     url,
                     method,
-                    null,
-                    new ParameterizedTypeReference<ResponseWrapper<T>>() {
-                    });
+                    entity,
+                    new ParameterizedTypeReference<ResponseWrapper<T>>() {},
+                    uriVariables);
 
+            // Get the response
             return response;
         } catch (Exception e) {
             Log.e("MainActivity", e.getMessage(), e);
