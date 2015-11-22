c04ad8c290379502e4dd4892f91815f48cb58046
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-11-22 01:56:25 -0500

    Move APIMeta out to it's own new class called ResponseMeta
    Note that all refactoring comes in the next commit titled "Document All The Things"

--- app/src/main/java/justbe/mindfulnessapp/rest/ResponseWrapper.java
+++ app/src/main/java/justbe/mindfulnessapp/rest/ResponseWrapper.java
@@ -67,51 +67,5 @@ public class ResponseWrapper <T> {
         return this.error;
     }
 
-    public class APIMeta {
-        private int limit;
-        private String next;
-        private int offset;
-        private String previous;
-        private int total_count;
-
-        public int getLimit() {
-            return limit;
-        }
-
-        public void setLimit(int limit) {
-            this.limit = limit;
-        }
-
-        public String getNext() {
-            return next;
-        }
-
-        public void setNext(String next) {
-            this.next = next;
-        }
-
-        public int getOffset() {
-            return offset;
-        }
-
-        public void setOffset(int offset) {
-            this.offset = offset;
-        }
-
-        public String getPrevious() {
-            return previous;
-        }
-
-        public void setPrevious(String previous) {
-            this.previous = previous;
-        }
-
-        public int getTotal_count() {
-            return total_count;
-        }
-
-        public void setTotal_count(int total_count) {
-            this.total_count = total_count;
-        }
     }
 }
