beee217dcbafe08c6f43a0a7c490a709b746a898
Colin Kohler <colink78@gmail.com>
2015-11-29 18:01:52 -0500

    Added lesson start, wake up, and sleep times to User object
    Still need to update database to return these new values.

--- app/src/main/java/justbe/mindfulnessapp/models/User.java
+++ app/src/main/java/justbe/mindfulnessapp/models/User.java
@@ -28,7 +28,10 @@ public class User extends BaseModel<User> {
     private String created_at;
     private String updated_at;
     private String start_date;
+    private String lesson_start;
     private String meditation_time;
+    private String wake_up_time;
+    private String go_to_sleep_time;
     private Integer gender;
 
     public String getEmail() {
@@ -119,13 +122,21 @@ public class User extends BaseModel<User> {
         this.start_date = start_date;
     }
 
-    public String getMeditation_time() {
-        return meditation_time;
-    }
+    public String getLesson_start() { return lesson_start; }
 
-    public void setMeditation_time(String meditation_time) {
-        this.meditation_time = meditation_time;
-    }
+    public void setLesson_start(String lesson_start) { this.lesson_start = lesson_start; }
+
+    public String getMeditation_time() { return meditation_time; }
+
+    public void setMeditation_time(String meditation_time) { this.meditation_time = meditation_time; }
+
+    public String getWake_up_time() { return wake_up_time; }
+
+    public void setWake_up_time(String wake_up_time) { this.wake_up_time = wake_up_time; }
+
+    public String getGo_to_sleep_time() { return this.go_to_sleep_time; }
+
+    public void setGo_to_sleep_time(String go_to_sleep_time) { this.go_to_sleep_time = go_to_sleep_time; }
 
     public Integer getGender() {
         return gender;
@@ -134,6 +145,4 @@ public class User extends BaseModel<User> {
     public void setGender(Integer gender) {
         this.gender = gender;
     }
-
-
 }
