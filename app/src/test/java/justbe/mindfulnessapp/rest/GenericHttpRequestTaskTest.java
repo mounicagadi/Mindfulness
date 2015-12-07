d01678a8232d51ca4fe1afac85610f6e41f2f0b1
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-12-07 09:50:36 -0500

    Move to test/ folder

--- app/src/test/java/justbe/mindfulnessapp/rest/GenericHttpRequestTaskTest.java
+++ app/src/test/java/justbe/mindfulnessapp/rest/GenericHttpRequestTaskTest.java
@@ -1,7 +1,5 @@
 package justbe.mindfulnessapp.rest;
 
-import android.support.test.espresso.assertion.ViewAssertions;
-import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import junit.framework.TestCase;
@@ -11,26 +9,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
-
-import justbe.mindfulnessapp.CreateAccountActivity;
-import justbe.mindfulnessapp.R;
 import justbe.mindfulnessapp.models.Assessment;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.clearText;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withHint;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static justbe.mindfulnessapp.ErrorTextMatcher.hasErrorText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.core.IsNull.notNullValue;
 
 /**
  * Created by eddiehurtig on 12/7/15.
@@ -53,7 +34,7 @@ public class GenericHttpRequestTaskTest extends TestCase {
 
         @Test
         public void testGetAssessment() {
-            GenericHttpRequestTask<String, Assessment> request = new GenericHttpRequestTask<String, Assessment>(String.class, Assesment.class);
+            GenericHttpRequestTask<String, Assessment> request = new GenericHttpRequestTask<String, Assessment>(String.class, Assessment.class);
 
             ResponseEntity<Assessment> response = request.doInBackground("/api/v1/assessment/5964/", HttpMethod.GET);
 
