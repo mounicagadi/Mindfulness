ad58b7b40b075ba7b563825b30ef2d5244dcc016
amitbanne <banne.a@husky.neu.edu>
2016-03-19 04:38:01 -0400

    MainActivity changes to load previous week contents

--- app/src/main/java/justbe/mindfulness/MainActivity.java
+++ app/src/main/java/justbe/mindfulness/MainActivity.java
@@ -3,15 +3,21 @@ package justbe.mindfulness;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -20,6 +26,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.ProgressBar;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,7 +46,7 @@ import justbe.mindfulness.models.User;
  *      - View Weekly Lesson
  *      - Go to Preferences
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     /**
      * Fields
@@ -50,9 +58,11 @@ public class MainActivity extends AppCompatActivity {
     private User user;
     private SessionManager sessionManager;
 
+    private ProgressDialog progressDialog;
+    private Handler handler;
     // audio player variables
     private MeditationMediaPlayer mediaPlayer;
-
+    private TextView lessonButtonText;
     /***********************************************************************************************
      * MainActivity Life Cycle Functions
      **********************************************************************************************/
@@ -88,14 +98,21 @@ public class MainActivity extends AppCompatActivity {
             session.setCsrfToken(sessionManager.getCSRFToken());
             session.setSessionId(sessionManager.getSessionID());
         }
+
+
         updateCurrentWeek();
         Integer selectedWeek = user.getCurrent_week();
 
+
+
+
         // Media player setup
-        mediaPlayer = new MeditationMediaPlayer(this, R.raw.sample, selectedWeek);
+        setUpMeditationContent(selectedWeek);
+        //mediaPlayer = new MeditationMediaPlayer(this, R.raw.sample, selectedWeek);
 
         // Set the lesson button's text to the current week
-        TextView lessonButtonText = (TextView) findViewById(R.id.weeklyLessonButtonText);
+        setUpLessonContent(selectedWeek);
+        /* TextView lessonButtonText = (TextView) findViewById(R.id.weeklyLessonButtonText);
         lessonButtonText.setText(String.format("Week %d Exercise", selectedWeek));
 
         // Get completed exercises from db, if this week has been completed give it a green check
@@ -113,15 +130,44 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
 
-        }
+        }*/
 //        // Pebble setup
 //        PebbleCommunicator comms = PebbleCommunicator.getInstance();
 //        if (!comms.checkPebbleConnection()) {
 //            Toast.makeText(App.context(), "No Pebble connection detected!", Toast.LENGTH_LONG).show();
 //        }
-//        setUpAlarms("assessment", 4, true);
+        setUpAlarms("assessment", 4, true);
 //        setUpAlarms("pebble", 5, false);
     }
+
+    public void setUpMeditationContent(int weekId){
+        mediaPlayer = new MeditationMediaPlayer(this, R.raw.sample, weekId);
+    }
+
+    public void setUpLessonContent(int weekId){
+
+        lessonButtonText = (TextView) findViewById(R.id.weeklyLessonButtonText);
+        lessonButtonText.setText(String.format("Week %d Exercise", weekId));
+
+        // Get completed exercises from db, if this week has been completed give it a green check
+        ExerciseSession[] completedExercises = ServerRequests.getExerciseSessions(this);
+        for (ExerciseSession e : completedExercises) {
+            if (e.getExercise_id() == weekId) {
+                int weeklyLessonImageId = getResources().getIdentifier(
+                        "weeklyLessonButtonImage", "id", getPackageName());
+                ImageView weeklyLessonImage = (ImageView) findViewById(weeklyLessonImageId);
+
+                // Change the lesson to be completed
+                weeklyLessonImage.setImageResource(R.drawable.check_green_2x);
+                weeklyLessonImage.setTag("true");
+
+                break;
+            }
+
+        }
+
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -179,13 +225,19 @@ public class MainActivity extends AppCompatActivity {
             popupWindow.setOutsideTouchable(true);
             popupWindow.setTouchable(true);
 
-            setupPopupTextFields(pw_view);
+            setupPopupTextFields(pw_view, popupWindow);
+            //pw_view.setOnClickListener(popupListener);
+            /*popupWindow.setTouchInterceptor(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
 
-            popupWindow.setTouchInterceptor(new View.OnTouchListener() {
-                @Override public boolean onTouch(View v, MotionEvent event) {
-                    popupWindow.dismiss(); return true;
+                    if (v instanceof TextView){
+                        Log.v("Popup","selected is a textview ");
+                    }
+                        //popupWindow.dismiss();
+                    return true;
                 }
-            });
+            });*/
 
             popupWindow.showAtLocation(pw_view, Gravity.CENTER, 0, 0);
         } catch (Exception e) {
@@ -203,6 +255,8 @@ public class MainActivity extends AppCompatActivity {
         ImageView checkMarkView = (ImageView) findViewById(checkMarkViewId);
         // The tag of the ImageView tells us if the lesson is completed or not
         Boolean checked = Boolean.valueOf(checkMarkView.getTag().toString());
+        String weekText  = lessonButtonText.getText().toString();
+        String week = weekText.split(" ")[1];
 
         // Change the lesson to be completed
         if (!checked) {
@@ -212,9 +266,31 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(MainActivity.this, LessonActivity.class);
         intent.putExtra("completed", checked);
+        intent.putExtra("week", week);
         intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         startActivity(intent);
     }
+
+    public void loadLessonContent(int weekId){
+        int checkMarkViewId = getResources().getIdentifier(
+                "weeklyLessonButtonImage", "id", getPackageName());
+        ImageView checkMarkView = (ImageView) findViewById(checkMarkViewId);
+        // The tag of the ImageView tells us if the lesson is completed or not
+        Boolean checked = Boolean.valueOf(checkMarkView.getTag().toString());
+
+        // Change the lesson to be completed
+        if (!checked) {
+            checkMarkView.setImageResource(R.drawable.check_green_2x);
+            checkMarkView.setTag("true");
+        }
+        /*System.out.println("Lesson Change Processing");
+        Intent intent = new Intent(MainActivity.this, LessonActivity.class);
+        intent.putExtra("completed", checked);
+        intent.putExtra("week", ""+weekId);
+        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intent);*/
+    }
+
 
     /**
      * Callback for when any weekday is pressed
@@ -296,18 +372,18 @@ public class MainActivity extends AppCompatActivity {
      * Sets the Text and Image fields on the weekly progress popup
      * @param pw_view The popup view that the fields are on
      */
-    private void setupPopupTextFields(View pw_view) {
+    private void setupPopupTextFields(View pw_view, PopupWindow popupWindow) {
 
-        int currentWeek = user.getCurrent_week();
-
+        final int currentWeek = user.getCurrent_week();
+        final PopupWindow pwindow = popupWindow;
         // Go through each week of the program and sets the correct UI
-        for(int i = 1; i <= 8; i++) {
+        for (int i = 1; i <= 8; i++) {
             TextView weekTextView = Util.getTextViewForWeek(pw_view, i);
             ImageView weekImageView = Util.getImageViewForWeek(pw_view, i);
-            if(i < currentWeek) {
+            if (i < currentWeek) {
                 weekTextView.setTextColor(ContextCompat.getColor(this, R.color.black));
                 weekImageView.setImageResource(R.drawable.check_green_2x);
-            } else if(i == currentWeek) {
+            } else if (i == currentWeek) {
                 weekTextView.setText(R.string.this_week_text);
                 weekTextView.setTypeface(null, Typeface.BOLD);
                 weekTextView.setTextColor(ContextCompat.getColor(this, R.color.black));
@@ -316,8 +392,126 @@ public class MainActivity extends AppCompatActivity {
                 weekTextView.setTextColor(ContextCompat.getColor(this, R.color.lightGray));
                 weekImageView.setImageResource(R.drawable.check_gray_2x);
             }
+
+
+            weekTextView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+
+                    final TextView tv = (TextView) v;
+                    String weekString = tv.getText().toString();
+                    weekString = weekString.replace(":", "");
+                    final String weekText = weekString.trim();
+
+                    progressDialog = new ProgressDialog(MainActivity.this);
+                    progressDialog.setTitle("Loading content for selected week");
+                    progressDialog.setMessage(getString(R.string.pleaseWait));
+                    progressDialog.setCancelable(false);
+
+                    class LoadContent extends AsyncTask {
+                        @Override
+                        protected void onPreExecute() {
+                            super.onPreExecute();
+                            progressDialog.show();
+                            pwindow.dismiss();
+                        }
+
+                        @Override
+                        protected Object doInBackground(Object[] params) {
+                            weekContentRequest(weekText, currentWeek);
+                            progressDialog.dismiss();
+                            return "";
+                        }
+
+                        @Override
+                        protected void onPostExecute(Object o) {
+                            super.onPostExecute(o);
+
+                            /*new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        Thread.currentThread().sleep(5000);
+                                        progressDialog.dismiss();
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }).start();*/
+
+
+                        }
+                    }
+
+
+                    new LoadContent().execute();
+                    return true;
+                }
+            });
+
+
         }
     }
+
+    public int getWeekId(String week, int currentWeek){
+
+        int weekID = 0;
+        switch(week){
+            case "This Week": weekID = currentWeek;
+                break;
+            case "Week 1": weekID = 1;
+                break;
+            case "Week 2": weekID = 2;
+                break;
+            case "Week 3": weekID = 3;
+                break;
+            case "Week 4": weekID = 4;
+                break;
+            case "Week 5": weekID = 5;
+                break;
+            case "Week 6": weekID = 6;
+                break;
+            case "Week 7": weekID = 7;
+                break;
+            case "Week 8": weekID = 8;
+                break;
+        }
+
+        return weekID;
+    }
+
+
+    public void  weekContentRequest(String weekSelected, final int currentWeek) {
+
+        final int weekId = getWeekId(weekSelected, currentWeek);
+        if (weekId <= currentWeek) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    populateWeekContent(weekId);
+                    loadLessonContent(weekId);
+                    Toast.makeText(getApplicationContext(),
+                            "Week "+weekId+" content loaded..! ", Toast.LENGTH_LONG).show();
+
+                }
+            });
+        }else{
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(),
+                            "Week "+currentWeek+" in progress.\nCannot Access content for Week "+weekId,Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+
+    public void populateWeekContent(int weekId) {
+        setUpMeditationContent(weekId);
+        setUpLessonContent(weekId);
+    }
+
 
     /***********************************************************************************************
      * Pebble Functions
@@ -332,29 +526,38 @@ public class MainActivity extends AppCompatActivity {
     private void setUpAlarms(String action, int count, boolean deleteOldAlarms) {
         Date userAwakeTime = user.getWake_up_time();
         Date userSleepTime = user.getGo_to_sleep_time();
-        if (userAwakeTime == null || userSleepTime == null) {
-            Toast toast = Toast.makeText(App.context(), "You need to set an awake time and a sleep time!", Toast.LENGTH_LONG);
-            toast.show();
-            return;
-        }
-        AlarmManager alarmManager = (AlarmManager)App.context().getSystemService(Context.ALARM_SERVICE);
-        if (deleteOldAlarms) {
-            PendingIntent cancelIntent = PendingIntent.getBroadcast(App.context(), 0,
-                    new Intent(App.context(), AlarmReceiver.class), 0);
-            alarmManager.cancel(cancelIntent);
-        }
-        Calendar[] alarmCals = CalenderGenerator.generateAwakeCalendars(count);
-        for (int i = 0; i < alarmCals.length; i++) {
-            Intent intent = new Intent(App.context(), AlarmReceiver.class);
-            intent.setAction(action + "|" + String.valueOf(alarmCals[i].getTimeInMillis()));
-            PendingIntent alarmIntent = PendingIntent.getBroadcast(App.context(),
-                    0,
-                    intent,
-                    0);
-            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,
-                    alarmCals[i].getTimeInMillis(),
-                    AlarmManager.INTERVAL_DAY,
-                    alarmIntent);
+        //Date userAwakeTime = null, userSleepTime = null;
+        try{
+            if (userAwakeTime == null || userSleepTime == null) {
+                Toast toast = Toast.makeText(App.context(), "You need to set an awake time and a sleep time!", Toast.LENGTH_LONG);
+                toast.show();
+                return;
+            }
+
+            AlarmManager alarmManager = (AlarmManager)App.context().getSystemService(Context.ALARM_SERVICE);
+            if (deleteOldAlarms) {
+                PendingIntent cancelIntent = PendingIntent.getBroadcast(App.context(), 0,
+                        new Intent(App.context(), AlarmReceiver.class), 0);
+                alarmManager.cancel(cancelIntent);
+            }
+            Calendar[] alarmCals = CalenderGenerator.generateAwakeCalendars(count);
+
+            for (int i = 0; i < alarmCals.length; i++) {
+                Intent intent = new Intent(App.context(), AlarmReceiver.class);
+                intent.setAction(action + "##" + String.valueOf(alarmCals[i].getTimeInMillis()));
+                PendingIntent alarmIntent = PendingIntent.getBroadcast(App.context(),
+                        0,
+                        intent,
+                        0);
+                alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,
+                        alarmCals[i].getTimeInMillis(),
+                        AlarmManager.INTERVAL_DAY,
+                        alarmIntent);
+
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+
         }
     }
 }
