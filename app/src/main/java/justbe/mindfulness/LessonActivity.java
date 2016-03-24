20ca6aaae9fbb555c9d5f276b8cead7d403e84d2
amitbanne <banne.a@husky.neu.edu>
2016-03-24 15:27:52 -0400

    Lesson Activity modified to remove pagination

--- app/src/main/java/justbe/mindfulness/LessonActivity.java
+++ app/src/main/java/justbe/mindfulness/LessonActivity.java
@@ -40,11 +40,7 @@ public class LessonActivity extends AppCompatActivity {
      */
 
     TextView lessonTitle, lessonContent;
-    private ViewPager mPager;
-    private FragmentPagerAdapter mPagerAdapter;
 
-    private List<String> pageContents;
-    LinearLayout mPageIndicator = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,7 +54,7 @@ public class LessonActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         lessonTitle =  (TextView) findViewById(R.id.lesson_title_text);
-
+        lessonContent = (TextView) findViewById(R.id.lesson_content);
 
         User user = App.getSession().getUser();
 
@@ -80,142 +76,16 @@ public class LessonActivity extends AppCompatActivity {
 
         Log.v("Lesson Activity", "" + weekId);
         lessonTitle.setText(weekExercise.getExerciseName());
+        lessonContent.setText(weekExercise.getExerciseContent());
 
-        //lessonContent.setText(text);
-
-
-        /**/
-        ViewGroup textviewPage = (ViewGroup) getLayoutInflater().inflate(R.layout.fragment, (ViewGroup) getWindow().getDecorView().findViewById(android.R.id.content), false);
-        lessonContent = null;
-        lessonContent =  (TextView) textviewPage.findViewById(R.id.text);
-        String contentString = weekExercise.getExerciseContent();
-
-        float horizontalMargin = getResources().getDimension(R.dimen.activity_horizontal_margin) * 2;
-        float verticalMargin = getResources().getDimension(R.dimen.activity_vertical_margin) * 2;
-
-        // obtaining screen dimensions
-        Display display = getWindowManager().getDefaultDisplay();
-        int screenWidth = (int) (display.getWidth() - horizontalMargin);
-        int screenHeight = display.getHeight();
-
-        TextPaint paint = lessonContent.getPaint();
-
-        //clear previous content
-        pageContents = new ArrayList<String>();
-
-        //Working Out How Many Lines Can Be Entered In The Screen
-        Paint.FontMetrics fm = paint.getFontMetrics();
-        float fullHeight = fm.top- fm.bottom-3;
-        fullHeight = Math.abs(fullHeight);
-
-
-
-        int numChars = 0;
-        int lineCount = 0;
-        int maxLineCount = (int) ((screenHeight - (/*actionBarHeight + */ verticalMargin) )/fullHeight);
-        /*maxLineCount-=2;*/
-        //contentTextView.setLines(maxLineCount);
-
-        // contentString is the whole string of the book
-        int totalPages = 0;
-        while (contentString != null && contentString.length() != 0 )
-        {
-            while ((lineCount < maxLineCount) && (numChars < contentString.length())) {
-                numChars = numChars + paint.breakText(contentString.substring(numChars), true, screenWidth, null);
-                lineCount ++;
-            }
-
-            // retrieve the String to be displayed in the current textbox
-            String toBeDisplayed = contentString.substring(0, numChars);
-            int nextIndex = numChars;
-            char nextChar = nextIndex < contentString.length() ? contentString.charAt(nextIndex) : ' ';
-            if (!Character.isWhitespace(nextChar)) {
-                toBeDisplayed = toBeDisplayed.substring(0, toBeDisplayed.lastIndexOf(" "));
-            }
-            numChars = toBeDisplayed.length();
-            contentString = contentString.substring(numChars);
-
-            pageContents.add(toBeDisplayed.trim());
-
-            numChars = 0;
-            lineCount = 0;
-
-            totalPages ++;
-        }
-        Log.v("TOTAL PAGES",""+totalPages);
-        // Instantiate a ViewPager and a PagerAdapter.
-        mPager = (ViewPager) findViewById(R.id.pager);
-        mPagerAdapter = new TestPagerAdapter(getSupportFragmentManager(), totalPages,"LessonActivity");
-        mPager.setAdapter(mPagerAdapter);
-        mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                showPageIndicator(position);
-            }
-        });
-
-        createPageIndicator();
-
-        /**/
-
-        if (!completed) {
+     if (!completed) {
             // TODO: get currently selected week instead of current program week
             int week = weekId;
             ServerRequests.completeExerciseSession(week, this);
         }
     }
 
-    @SuppressWarnings("deprecation")
-    private void createPageIndicator() {
-        mPageIndicator = (LinearLayout) findViewById(R.id.pageIndicator);
-        for (int i = 0; i < pageContents.size(); i++) {
-            View view = new View(this);
-            ViewGroup.LayoutParams params = new TableLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f);
-            view.setLayoutParams(params );
-            view.setBackgroundDrawable(getResources().getDrawable(i == 0 ? R.drawable.current_page_indicator : R.drawable.indicator_background));
-            view.setTag(i);
-            mPageIndicator.addView(view);
-        }
-    }
 
-    @SuppressWarnings("deprecation")
-    protected void showPageIndicator(int position) {
-        try {
-            mPageIndicator = (LinearLayout) findViewById(R.id.pageIndicator);
-            for (int i = 0; i < pageContents.size(); i++) {
-                View view = mPageIndicator.findViewWithTag(i);
-                view.setBackgroundDrawable(getResources().getDrawable(i == position ? R.drawable.current_page_indicator : R.drawable.indicator_background));
-            }
-        } catch (Exception e) {
-           // Log.e(TAG, e.toString());
-        }
-    }
-
-    public String getContents(int pageNumber){
-        return pageContents.get(pageNumber);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lesson, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
 
 }
